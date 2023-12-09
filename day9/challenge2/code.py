1947bb5a403f48606d1b9836da16f97c4351e3a8
SipuliCodes <vepsalainensisu@gmail.com>
2023-12-09 12:11:48 +0200

    day9_completed

---
+++ day9/challenge2/code.py
@@ -0,0 +1,22 @@
+def extrapolate(numbers: list):
+    biggest_from_rows = []
+    while numbers:
+        for index, value in enumerate(numbers):
+            try:
+                numbers[index] = numbers[index + 1] - value
+            except:
+                pass
+        biggest_from_rows.append(numbers.pop())
+    return sum(biggest_from_rows)
+
+def make_int(numbers: list):
+    return [int(number) for number in numbers]
+
+def reverted_list(numbers: list):
+    return numbers[::-1]
+
+with open("day9/input.txt") as input:
+    sums = []
+    for line in input:
+        sums.append(extrapolate(reverted_list(make_int(line.split()))))
+    print(sum(sums))
